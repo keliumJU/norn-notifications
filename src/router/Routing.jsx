da93bfc37e0464d3249f64fae7e27049502054c8
keliumJU <keliumju@gmail.com>
2021-11-13 05:27:25 -0500

    routing bug

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -14,7 +14,7 @@ function Routing() {
 		const [logged] = useAuth();
 		const token = JSON.parse(localStorage.getItem("REACT_TOKEN_AUTH_KEY"))
 		const user = jwt(token.access_token)
-		
+
 		if(logged){
 			if(roles && roles.indexOf(user.rls)===-1){
 				return <Route {...rest} render={(props) => (
@@ -37,11 +37,11 @@ function Routing() {
 		return (
 			<>
 				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/login" exact component={Login} />
-					<Route path="/register" exact component={Register} />
-					<PrivateRoute path="/secret" roles={[Role.Graduate, Role.Admin]} exact component={Secret} />
-					<PrivateRoute path="/user" roles={[Role.Admin]} exact component={User} />
+					<Route path="norn-notifications/" exact component={Home} />
+					<Route path="norn-notifications/login" exact component={Login} />
+					<Route path="norn-notifications/register" exact component={Register} />
+					<PrivateRoute path="norn-notifications/secret" roles={[Role.Graduate, Role.Admin]} exact component={Secret} />
+					<PrivateRoute path="norn-notifications/user" roles={[Role.Admin]} exact component={User} />
 			</Switch>
 		</>
 	)
