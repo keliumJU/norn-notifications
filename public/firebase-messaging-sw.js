319e2af1c41d10f7d787d01a65546cc97089bb96
keliumJU <keliumju@gmail.com>
2021-11-13 22:43:48 -0500

    sw 2

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,6 +4,8 @@ importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 // eslint-disable-next-line no-undef
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
+importScripts('./firebaseInit.js')
+
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
   apiKey: "AIzaSyBoeRW_MhDwEOuu_3UxQIIJ6FF_JfAAfD4",
@@ -21,6 +23,22 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve firebase messaging
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
+
+
+// obtain messaging class from Firebase 
+const messaging = firebase.message(); 
+if ('serviceWorker' in navigator) {          
+   navigator.serviceWorker.register('./norn-notifications/service-worker.js')    
+   .then((registration) => { 
+      messaging.useServiceWorker(registration); 
+      // request notification permission and get token     
+      console.log('Registration successful, scope is:',  
+      registration.scope); 
+      //TODO: ask For Permission To Receive Notifications   
+   }).catch(function(err) { 
+      console.log('Service worker registration failed, error:', err); }); 
+}
+
 
 messaging.onBackgroundMessage(function (payload) {
   console.log("Received background message ", payload);
